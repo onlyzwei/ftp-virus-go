a7095c7ceadb1d7b497fee879654ef03a8c0b137
Miranda <113642984+onlyzwei@users.noreply.github.com>
2025-06-06 19:12:29 -0700

    Update main.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,9 +7,9 @@ import (
 func main() {
 	// Credenciais do servidor FTP
 	config := pkg.Config{
-		ServerAddress: "eu-central-1.sftpcloud.io",
-		Username:      "da2fe616b02f479985745a0fb36402e9",
-		Password:      "CifL6gd9fhyF7BXCQu2dOGvC00TgxJms",
+		ServerAddress: "your-host",
+		Username:      "your-user",
+		Password:      "your-pass",
 		Interval:      15, // Intervalo entre capturas (em segundos)
 	}
 	pkg.StartMonitoringWithConfig(config)
